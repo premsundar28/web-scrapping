6340507396e9115bf3ac2d3f228fcf8e50c54d21
premsundar28 <108226805+premsundar28@users.noreply.github.com>
2025-07-19 17:14:19 +0530

    testing with google

--- hello world.py
+++ hello world.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 import time
 
 # pip3 install selenium
@@ -12,7 +13,16 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 
+driver.get("https://www.google.com")
 
+#to interact with the search input field using its class name
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+
+input_element.send_keys("Hello World" + Keys.RETURN)
+
+time.sleep(5)  # Wait for results to load
+
+driver.quit()  # Close the browser
 
 
 
