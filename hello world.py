f474b30eb061c8e907665bec047a7450ee64cbe8
premsundar28 <108226805+premsundar28@users.noreply.github.com>
2025-07-19 16:54:47 +0530

    add comments for packages and installation process

--- hello world.py
+++ hello world.py
@@ -4,10 +4,14 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-
+# pip3 install selenium
+# pip3 install webdriver-manager
+# to run this script , use "python3 hello world.py"
 # Automatically downloads and sets up ChromeDriver
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
+
+
 
 
 
